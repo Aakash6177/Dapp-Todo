2dc750ebf8e0494ddf5f3f56ac21fc6a27fbaa38
Aakash6177 <www.aakash7469@gmail.com>
2023-02-23 12:09:39 -0800

    first commit

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,22 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-    uint256 public taskCount = 0;
+    uint256 public taskCount = 0; // this is a state variable, being written to storage
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks; // this is an associative array like maps in C++ [(key:"value"), (key:"value")]
+
+    constructor() public {
+        createtask("Complete REST API homework");
+    }
+
+    function createtask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
